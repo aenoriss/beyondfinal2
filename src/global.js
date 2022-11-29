739e04ddc06798eace36074eb89dd762451387c3
aenoris <aenoriss@gmail.com>
2022-11-29 11:05:34 -0300

    dasasd

--- src/global.js
+++ src/global.js
@@ -1,12 +1,6 @@
 // Mandatory A-Frame import
 require('aframe');
 
-// Additional optional components
-require('aframe-animation-component');
-require('aframe-event-set-component');
-require('aframe-layout-component');
-require('aframe-template-component');
-
 // Custom components in components/
 // require('./components/foo');
 require('./components/transcription');
